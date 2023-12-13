dc8a4144543467b33382c378ca29fea739d3d571
Harshit Goyal <HarshGoyal1357@gmail.com>
2023-12-14 00:41:12 +0530

    Add files via upload

---
+++ House.java
@@ -0,0 +1,44 @@
+public class House implements Cloneable, Comparable<House> {
+    private int id;
+    private double area;
+    private java.util.Date whenBuilt;
+
+    public House(int id, double area) {
+        this.id = id;
+        this.area = area;
+        whenBuilt = new java.util.Date();
+    }
+
+    // Clone arrays
+    // How to implement Cloneable
+
+    public int getId() {
+        return id;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public java.util.Date getWhenBuilt() {
+        return whenBuilt;
+    }
+
+    @Override
+    /** Override the protected clone method defined in
+     * the Object class, and strengthen its accessibility */
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    // Implement the compareTo method defined in Comparable
+    public int compareTo(House o) {
+        if (area > o.area)
+            return 1;
+        else if (area < o.area)
+            return -1;
+        else
+            return 0;
+    }
+}
